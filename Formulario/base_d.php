5c940871774d1c369531f3de1ddb6f5b7be33ede
Jos <118079175+PanditaGeek@users.noreply.github.com>
2024-04-24 16:07:35 -0500

    Update base_d.php
    cambio en la variable

--- Formulario/base_d.php
+++ Formulario/base_d.php
@@ -1,10 +1,5 @@
 <?php 
-$mysqli = new mysqli("127.0.0.1", "root", "", "basedatos", 3306);
-if ($mysqli->connect_errno) {
-    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
+$conex  = new mysqli("127.0.0.1", "root", "", "basedatos", 3306);
 
-echo $mysqli->host_info . "\n";
-?>
 
 //Crear base de datos en XAMP
